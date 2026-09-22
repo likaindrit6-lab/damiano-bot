728ffe5155bb7bb19ce4d845eb942cd5e4b1f142
likaindrit6-lab <likaindrit6@gmail.com>
2026-09-22 22:23:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
+
 import os
 from telegram.ext import Application, CommandHandler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("Bot ONLINE!")
+    await update.message.reply_text("🔥 DAMI BOT OK! 09:00-01:00 attivo")
 
 async def live(update, context):
-    await update.message.reply_text("Funziona Dami!")
+    await update.message.reply_text("Sono vivo Dami! Nessun LIVE ora")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
