d2013f70837dd371c43099187c19f53794e7d9ca
likaindrit6-lab <likaindrit6@gmail.com>
2026-09-22 22:57:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
-
 import os
 from telegram.ext import Application, CommandHandler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("🔥 DAMI BOT OK! SONO LIVE! Ora funziona!")
+    await update.message.reply_text("🔥 DAMI BOT OK! SONO LIVE!")
 
 async def live(update, context):
     await update.message.reply_text("Sono vivo Dami!")
@@ -14,6 +13,7 @@ def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("live", live))
+    print("Bot avviato...")
     app.run_polling()
 
 if __name__ == "__main__":
